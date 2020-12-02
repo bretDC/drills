43f9afad727edf2bd4f05f86aa484c2dcceab7df
bretThinkful <bretbickhamx1989@gmail.com>
2020-12-02 14:18:33 -0700

    drills

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello World</h2>
+        <HelloWorld />
+        <h2>Bomb</h2>
+        <Bomb />
+        <h2>Roulette Gun</h2>
+        <RouletteGun bulletInChamber={6} />
+      </div>
+    )
+  }
+}
+
+export default App;
